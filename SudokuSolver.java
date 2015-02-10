7dbfb4bf12d669380e5619385fa4b4a0ea6fa380
2ee657f88f6f82ff597c3abd18b96a3a11355177 <ianchipperfield@gmail.com>
2015-02-09 23:30:07 -0800

    Added names, template for arc consistency search. Google doc now specifies constraints this will handle..

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -9,8 +9,13 @@ public class SudokuSolver {
 	 * @return names of the authors and their student IDs (1 per line).
 	 */
 	public String authors() {
-		// TODO write it;
-		return "NAMES OF THE AUTHORS AND THEIR STUDENT IDs (1 PER LINE)";
+		/*
+		Name (last, first):  Chipperfield, Ian
+		Student ID: 94393071
+		Partner’s Name (last, first): Jeffery, Jon
+		Student ID: 26434126
+		*/
+		return "Ian Chipperfield 94393071\nJon Jeffery 26434126";
 	}
 
 	/**
@@ -22,5 +27,40 @@ public class SudokuSolver {
 	public int[][] solve(int[][] board) {
 		// TODO write it;
 		return board;
+	}
+
+	// Successor Function:
+	// reduce a domain and run arc consistency
+	public void successor(){
+
+	}
+
+	public void checkArcConsistency() {
+		/*
+		Procedure GAC(V,dom,C)
+			Inputs
+				V: a set of variables
+				dom: a function such that dom(X) is the domain of variable X
+				C: set of constraints to be satisfied
+			Output
+				arc-consistent domains for each variable
+			Local
+				DX is a set of values for each variable X
+				TDA is a set of arcs
+
+			for each variable X do DX ←dom(X)
+				TDA ←{〈X,c〉| X ∈ V, c ∈ C and X ∈ scope(c)}
+
+			while (TDA != {})
+				select 〈X,c〉 ∈TDA
+				TDA ←TDA \ {〈X,c〉}
+				NDX ←{x|x∈DX and for some y∈DY s.t.(x,y) satisfies c}
+				if (NDX != DX) then
+					TDA ←TDA ∪ { 〈Z,c'〉 | X ∈ scope(c'), c' != c, Z ∈ scope(c') \ {X} }
+					DX ←NDX
+
+		return {DX| X is a variable}
+		*/
+
 	}
 }
