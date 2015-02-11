c38083fedd1c369d7d82819be35eb6ab8b415ffc
2ee657f88f6f82ff597c3abd18b96a3a11355177 <ianchipperfield@gmail.com>
2015-02-11 15:34:02 -0800

    Added checks in successor for empty/unary/multi-value grid states

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -10,7 +10,11 @@ import java.util.LinkedList;
 public class SudokuSolver {
 
 	public static int BOARD_SIZE = 9;
+	public static int TOTAL_DOMAINS = BOARD_SIZE*BOARD_SIZE;
 	public static int TIMESREDUCED = 0;
+
+	LinkedList<Domains> solutions = new LinkedList<Domains>();
+	LinkedList<Domains> toDo = new LinkedList<Domains>();
 
 	/**
 	 * @return names of the authors and their student IDs (1 per line).
@@ -33,71 +37,155 @@ public class SudokuSolver {
 	 */
 	public int[][] solve( int[][] board ) {
 		Domains start = new Domains( board );
-		checkArcConsistency(start);
-		System.out.println(TIMESREDUCED);
+		checkArcConsistency( start );
+		solutions.clear();
+		toDo.clear();
 
+		toDo.push( start );
+		Domains current = null;
 
+		while ( !toDo.isEmpty() ){
+			current = toDo.peekFirst();
 
-		return start.toBoard();
-		//return board;
+			// iterate through all variables in board
+			for ( int i = 0; i < BOARD_SIZE; i++ ){
+				for ( int j = 0; j < BOARD_SIZE; j++ ){
+					successor( start, i , j );
+				}
+			}
+
+		}
+		return solutions.peekFirst().toBoard();
 	}
 
 	// reduce a domain and run arc consistency
-	public void successor(){
-		// check if current domain is empty
+	public void successor( Domains current, int row, int col ){
+
 		// check if domain is unary
-		// if multiple values, split
+		if ( isSolution(current) ) {
+			toDo.pop();
+			solutions.push(current);
+			return;
+		} else if ( hasInvalidDomain( current ) ) {
+			toDo.pop();
+			return;
+		}
+
+		// Variable Has Multiple Values
+		else {
+			Domains firstHalf = current;
+			Domains secondHalf = current;
+			int domainLength = current.grid[row][col].size();
+
+			LinkedList<Integer> first = current.grid[row][col];
+			for ( int i = 0; i < domainLength / 2; i++ ){
+				first.removeLast();
+			}
+
+			LinkedList<Integer> second = current.grid[row][col];
+			for ( int i = ( domainLength / 2 ); i < domainLength; i++ ){
+				second.removeFirst();
+			}
+
+			firstHalf.grid[row][col] = first;
+			secondHalf.grid[row][col] = second;
+
+			checkArcConsistency( firstHalf );
+			checkArcConsistency( secondHalf );
+
+			toDo.push( firstHalf );
+			toDo.push( secondHalf );
+		}
+	}
+
+	public boolean isSolution( Domains current ){
+		// check if all domains are unary
+		boolean allUnary = true;
+		for ( int i = 0; i < BOARD_SIZE; i++ ){
+			for ( int j = 0; j< BOARD_SIZE; j++ ){
+				if ( current.grid[i][j].size() != 1 ) allUnary = false;
+			}
+		}
+		return allUnary;
+	}
+
+	public boolean hasInvalidDomain( Domains current ){
+		boolean hasInvalidDomain = false;
+		for ( int i = 0; i < BOARD_SIZE; i++ ){
+			for ( int j = 0; j< BOARD_SIZE; j++ ){
+				if (current.grid[i][j].size() == 0 ) hasInvalidDomain = true;
+			}
+		}
+		return hasInvalidDomain;
 	}
 
 	public void checkArcConsistency( Domains grid ) {
-		for (int i = 0; i< BOARD_SIZE; i++){
-			for (int j = 0; j< BOARD_SIZE; j++){
-				checkConsistency( grid, i, j );
+		boolean done = false;
+
+		while ( !done ) {
+			int numberConsistentDomains = 0;
+			boolean consistent = false;
+
+			for ( int i = 0; i < BOARD_SIZE; i++ ){
+				for ( int j = 0; j < BOARD_SIZE; j++ ){
+					consistent = checkConsistency( grid, i, j );
+					if ( consistent ) numberConsistentDomains++;
+				}
 			}
+			if ( numberConsistentDomains == TOTAL_DOMAINS) done = true;
 		}
 		//returns consistent input
 		// reduce all domains to empty if
 	}
 
-	public void checkConsistency( Domains grid, int i, int j ){
+	// Return true if this variable's domain is consistent across all constraints
+	public boolean checkConsistency( Domains grid, int i, int j ){
 		// check row values
-		checkRow( grid, i, j );
+		boolean rowConsistent = checkRow( grid, i, j );
 		// check column values
-		checkColumn( grid, i, j );
+		boolean colConsistent = checkColumn( grid, i, j );
 		// check sub-square values
-		checkSubSquare( grid, i, j );
+		boolean subSquareConsistent = checkSubSquare( grid, i, j );
+
+		return ( rowConsistent && colConsistent && subSquareConsistent );
 	}
 
-	public void checkRow( Domains grid, int i, int j ){
+	// return true if row constraint is satisfied
+	public boolean checkRow( Domains grid, int i, int j ){
+		boolean consistent = true;
 		for ( int k = 0; k < BOARD_SIZE; k++ ){
 			if ( k != j ) {
 				if ( grid.grid[i][k].size() == 1 ) {
 					int toRemove = grid.grid[i][k].getFirst();
-					if ( grid.grid[i][j].contains( toRemove ) ){
-						grid.grid[i][j].removeFirstOccurrence( toRemove );
+					if ( grid.grid[i][j].removeFirstOccurrence( toRemove ) ){
 						TIMESREDUCED++;
+						consistent = false;
 					}
 				}
 			}
 		}
+		return consistent;
 	}
 
-	public void checkColumn( Domains grid, int i, int j ){
+	public boolean checkColumn( Domains grid, int i, int j ){
+		boolean consistent = true;
 		for ( int k = 0; k < BOARD_SIZE; k++ ){
 			if ( k != i ) {
 				if ( grid.grid[k][j].size() == 1 ) {
 					int toRemove = grid.grid[k][j].getFirst();
-					if ( grid.grid[i][j].contains( toRemove ) ){
-						grid.grid[i][j].removeFirstOccurrence(toRemove);
+					if ( grid.grid[i][j].removeFirstOccurrence(toRemove) ){
 						TIMESREDUCED++;
-
+						consistent = false;
 					}
 				}
 			}
 		}
+		return consistent;
 	}
 
-	public void checkSubSquare( Domains domain, int row, int col ){
+	public boolean checkSubSquare( Domains domain, int row, int col ){
+
+		boolean consistent = true;
 
 		int rowGroup = row / 3;
 		int colGroup = col / 3;
@@ -110,13 +198,15 @@ public class SudokuSolver {
 				if ( !( i == row && j == col ) ){
 					if ( domain.grid[i][j].size() == 1 ){
 						int toRemove = domain.grid[i][j].getFirst();
-						domain.grid[row][col].removeFirstOccurrence( toRemove );
-						TIMESREDUCED++;
-
+						if ( domain.grid[row][col].removeFirstOccurrence( toRemove ) ){
+							TIMESREDUCED++;
+							consistent = false;
+						}
 					}
 				}
 			}
 		}
+		return consistent;
 	}
 
 	public class Domains {
@@ -126,9 +216,9 @@ public class SudokuSolver {
 			for (int i = 0; i < BOARD_SIZE; i++){
 				for (int j = 0; j < BOARD_SIZE; j++){
 					if (board[i][j] == 0){
-						grid[i][j] = new LinkedList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+						grid[i][j] = new LinkedList<Integer>( Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9 ));
 					} else {
-						grid[i][j] = new LinkedList<Integer>(Arrays.asList(board[i][j]));
+						grid[i][j] = new LinkedList<Integer>( Arrays.asList( board[i][j] ) );
 					}
 				}
 			}
